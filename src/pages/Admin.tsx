877ede909444661c28f1741a727744fec6e05e33
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-05-26 11:08:08 -0300

    Fix: Itens finais

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -404,7 +404,7 @@ useEffect(() => {
 
             <div className="flex items-center gap-3">
               <div className="w-11 h-11 rounded-full bg-[#d8ece7] flex items-center justify-center font-bold text-[#014d4e]">
-                AL
+                AD
               </div>
 
               <div>
